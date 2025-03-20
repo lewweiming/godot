e4f0bbd380f42af2cfb901ca450adc538c950e60
Kevin.L <kevinlew27@gmail.com>
2025-03-20 23:44:59 +0800

    Create basic_2d_top_down_space_shooter_motion.gd

---
+++ scripts/basic_2d_top_down_space_shooter_motion.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@onready var animatedSprite := $AnimatedSprite
+
+@export var speed: float = 100
+var v = Vector2(0,0)
+
+func _process(delta):
+	# Animate
+	if v.x < 0:
+		animatedSprite.play("Left")
+	elif v.x > 0:
+		animatedSprite.play("Right")
+	else:
+		animatedSprite.play("Straight")
+
+func _physics_process(delta):
+	var dirVec = Vector2(0,0)
+	if Input.is_action_pressed("move_left"):
+		dirVec.x = -1
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x = 1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y = 1
+	
+	v = dirVec.normalized() * speed	
+	position += v *delta
+	
+	# Make sure that we are within the screen
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.y = clamp(position.y, 0, viewRect.size.y)
